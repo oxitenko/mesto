fc6fabe3d024750ee856ddbc1ab969d0c1acd4dd
Oksana Komarova <rabota_komarova@mail.ru>
2022-03-27 10:52:48 +0500

    feat: popup

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let modalWindow = document.querySelector('.popup');
+let addButton = document.querySelector('.button__edit');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__profi');
+
+function OpenCloseModalWindow() {
+    modalWindow.classList.toggle('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    OpenCloseModalWindow();
+}
+
+addButton.addEventListener('click', OpenCloseModalWindow);
+closeButton.addEventListener('click', OpenCloseModalWindow);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
