68bbecdc5381df2bd7f1f56d0f07a6d632c90dcb
Oksana Komarova <rabota_komarova@mail.ru>
2022-03-31 10:35:12 +0500

    fix: исправлены ошибки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,18 @@
 let modalWindow = document.querySelector('.popup');
-let addButton = document.querySelector('.profile__button-edit');
+let editButton = document.querySelector('.profile__button-edit');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let nameInput = document.querySelector('.popup__input_enter_name');
+let jobInput = document.querySelector('.popup__input_enter_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__profi');
 
 function OpenCloseModalWindow() {
+    if (!modalWindow.classList.contains('popup_opened')) {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileJob.textContent;    
+    }
     modalWindow.classList.toggle('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
 }
 
 function formSubmitHandler (evt) {
@@ -21,11 +23,9 @@ function formSubmitHandler (evt) {
     OpenCloseModalWindow();
 }
 
-addButton.addEventListener('click', OpenCloseModalWindow);
+editButton.addEventListener('click', OpenCloseModalWindow);
 closeButton.addEventListener('click', OpenCloseModalWindow);
 formElement.addEventListener('submit', formSubmitHandler);
-
-
 
 
 
